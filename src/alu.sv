a0a77356e43ca2c9877100b5804159ea83cf109c
Adam14adam <adam14lukic@gmail.com>
2026-01-05 21:09:47 +1100

    Refined logic for alu and rf

--- src/alu.sv
+++ src/alu.sv
@@ -1,8 +1,7 @@
 module ALU_16 (
-    input  logic        clk,
-    input  logic [15:0] in1,
-    input  logic [15:0] in2,
-    input  logic [2:0]  alu_sel,
+    input logic [15:0] in1,
+    input logic [15:0] in2,
+    input logic [2:0] alu_sel,
     output logic [15:0] alu_out
 );
 
@@ -15,17 +14,17 @@ module ALU_16 (
                LSR  = 3'b110,
                LSL  = 3'b111;
 
-    always_ff @(posedge clk) begin
+    always_comb begin
         case (alu_sel)
-            ADD:  alu_out <= in1 + in2;
-            SUB:  alu_out <= in1 - in2;
-            AND_: alu_out <= in1 & in2;
-            OR_:  alu_out <= in1 | in2;
-            XOR_: alu_out <= in1 ^ in2;
-            FLIP: alu_out <= in1 ^ (16'b1 << in2[3:0]); // flip one bit
-            LSR:  alu_out <= in1 >> in2[3:0];
-            LSL:  alu_out <= in1 << in2[3:0];
-            default: alu_out <= 16'h0000;
+            ADD: alu_out = in1 + in2;
+            SUB: alu_out = in1 - in2;
+            AND_: alu_out = in1 & in2;
+            OR_: alu_out = in1 | in2;
+            XOR_: alu_out = in1 ^ in2;
+            FLIP: alu_out = in1 ^ (16'b1 << in2[3:0]); // flip one bit
+            LSR: alu_out = in1 >> in2[3:0];
+            LSL: alu_out = in1 << in2[3:0];
+            default: alu_out = 16'h0000;
         endcase
     end
 
--- src/rf.sv
+++ src/rf.sv
@@ -1,45 +1,47 @@
 module rf (
-    input wire clk,
+    input logic clk,
 
-	input wire [15:0] w_data,
-    input wire [3:0] w_addr,
-    input wire w_en,
+    input logic [15:0] w_data,
+    input logic [3:0] w_addr,
+    input logic w_en,
 
-    input wire [15:0] pc_data,
-    input wire [3:0] pc_addr,
-    input wire pc_en,
+    input logic [15:0] pc_data,
+    input logic [3:0] pc_addr,
+    input logic pc_en,
 
-    input wire [3:0] out1_addr,
-    input wire out1_en,
+    input logic [3:0] out1_addr,
+    input logic out1_en,
 
-    input wire [3:0] out2_addr,
-    input wire out2_en,
+    input logic [3:0] out2_addr,
+    input logic out2_en,
 
-    output wire [15:0] out1,
-    output wire [15:0] out2
+    output logic [15:0] out1,
+    output logic [15:0] out2
 );
 
     // 16 x 16-bit register file
-    reg [15:0] mem [0:15];
+    logic [15:0] rf_reg [0:15];
 
     // -------------------------
     // Write logic (clocked)
     // PC has priority
     // -------------------------
-    always @(posedge clk) begin
+    always_ff @(posedge clk) begin
         if (pc_en) begin
-            mem[pc_addr] <= pc_data;
+            rf_reg[pc_addr] <= pc_data;
         end
         else if (w_en) begin
-            mem[w_addr] <= w_data;
+            rf_reg[w_addr] <= w_data;
         end
     end
 
     // -------------------------
     // Read logic (combinational)
     // -------------------------
-    assign out1 = out1_en ? mem[out1_addr] : 16'b0;
-    assign out2 = out2_en ? mem[out2_addr] : 16'b0;
+    always_comb begin
+        assign out1 = out1_en ? mem[out1_addr] : 16'b0;
+        assign out2 = out2_en ? mem[out2_addr] : 16'b0;
+    end
 
 endmodule
 
